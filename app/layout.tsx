3a388726cd802d03f3caa6225f9d4e53e6a53c7b
admingistai <sharedgistai@gmail.com>
2025-10-09 13:09:51 -0700

    Add widget script back

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,27 +82,12 @@ export default function RootLayout({
         </noscript>
         {/* End Meta Pixel Code */}
 
-        {/* TVScientific Pixel */}
-        <Script id="tvscientific-pixel" strategy="afterInteractive">
-          {`
-            (function () {
-              var p, s, d, w;
-              d = document;
-              w = window.location;
-              p = d.createElement("IMG");
-              s = w.protocol + "//tvspix.com/t.png?&t=" + (new Date).getTime() + "&l=tvscientific-pix-o-21b0ba9e-3013-4fd3-bdee-8b53298efcd4&u3=" + encodeURIComponent(w.href);
-              p.setAttribute("src", s);
-              p.setAttribute("height", "0");
-              p.setAttribute("width", "0");
-              p.setAttribute("alt", "");
-              p.style.setProperty("display", "none");
-              p.style.setProperty("position", "absolute");
-              p.style.setProperty("visibility", "hidden");
-              d.body.appendChild(p);
-            })();
-          `}
-        </Script>
-        {/* End TVScientific Pixel */}
+
+        {/* Widget Script */}
+        <Script
+          src="https://widget-deploy-hazel.vercel.app/widget.js"
+          strategy="afterInteractive"
+        />
 
         {/* Amplitude Analytics */}
         <Script id="amplitude-init" strategy="afterInteractive">
